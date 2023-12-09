eeebe54423e30092b00ebb5ae7988a0cf42fdf61
pavan <siriyalapavankumar@gmail.com>
2023-12-09 23:56:42 +0530

    done with payment component✅

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -1,8 +1,8 @@
 
 const Payment = () => {
   return (
-    <div className="flex flex-col items-center gap-7 pt-6 text-center">
-      <div className="flex flex-col sm:flex-row gap-4  sm:gap-7">
+    <div className="flex flex-col items-center gap-7 pt-6 text-center ">
+      <div className="flex flex-col sm:flex-row gap-4  sm:gap-7 ">
         <a
           href="https://apps.apple.com/us/app/sleeve/id1606145041"
           className="flex items-center justify-center gap-3 px-6 h-14 font-semibold text-lg text-white dark:text-black bg-black dark:bg-white active:bg-black hover:bg-[#333] dark:active:bg-gray-200 dark:hover:bg-gray-100 border border-black/20 dark:border-white shadow active:shadow-black/0 rounded-xl transition">
@@ -25,7 +25,8 @@ const Payment = () => {
           </p>
       </div>
       <p className="pt-6 mb-12">
-        <a className="flex items-center max-w-[540px] lg:max-w-full text-gray-700 dark:text-white/80 bg-gray-50 dark:bg-transparent hover:bg-white dark:hover:bg-white/5 shadow-outline dark:shadow-outline-superbright px-4 py-3 gap-3 rounded-3xl transition" href="/updates/sleeve-2-2-for-sonoma">
+        <a className="flex items-center max-w-[540px] lg:max-w-full text-gray-700 dark:text-white/80 bg-gray-50 dark:bg-transparent hover:bg-white dark:hover:bg-white/5 shadow-outline dark:shadow-outline-superbright px-4 py-3 gap-3 rounded-3xl transition"
+          href="/updates/sleeve-2-2-for-sonoma">
           <img src="https://replay.software/sleeve/images/Sonoma.png"
             width="40" height="40" alt="macOS Sonoma Icon"
             className="-m-3 -ml-2.5 -mr-1.5">
@@ -33,7 +34,7 @@ const Payment = () => {
           <span className="sm:hidden">Updated for macOS Sonoma
         </span>
           <span className="hidden sm:inline-block">
-            <span className="hidden sm:inline-block">
+            <span className="hidden sm:inline-block space-x-0">
               Now updated
             </span>
             <span className="sm:hidden">
