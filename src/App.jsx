08ee2b6a87a5e33ef93d4fd6165ec83295bce489
pavan <siriyalapavankumar@gmail.com>
2023-12-09 14:58:43 +0530

    done with header part✅

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Home from "./components/Home"
+
+
+const App = () => {
+  return (
+    <main className= "">
+
+     <Home />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+
+const Home = () => {
+  return (
+    <div>
+          <h1 className="relative text-center text-6xl md:text-8xl font-bold tracking-tight bg-gradient-to-b from-black to-[#444] bg-clip-text text-transparent dark:from-white dark:to-[#ccc] z-20 pb-8">
+              Sleeve
+              <span className="font-light">2</span>
+          </h1>
+          <p className=" relative text-2xl sm:text-5xl font-bold max-w-[20ch] lg:max-w-full text-white pl-64">
+              The ultimate music accessory for your Mac.
+          </p>
+    </div>
+  )
+}
+
+export default Home
