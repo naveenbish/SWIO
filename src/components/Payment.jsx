7f53d57911c00fc0e1e1029363746ce21f219dce
pavan <siriyalapavankumar@gmail.com>
2023-12-11 13:19:24 +0530

    done with Music page✅

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -1,5 +1,8 @@
+import { Link } from "react-router-dom"
+
 
 const Payment = () => {
+  
   return (
     <div className="flex flex-col items-center gap-7 pt-6 text-center  ">
       <div className="flex flex-col sm:flex-row gap-4  sm:gap-7 mb-10">
@@ -15,7 +18,12 @@ const Payment = () => {
           Mac App Store
         </a>
         <button className="relative flex items-center justify-center gap-3 px-6 h-14 font-semibold text-lg bg-gray-50 dark:bg-transparent active:bg-gray-100 dark:active:bg-white/0 hover:bg-white dark:hover:bg-white/5 border border-black/10 dark:border-white/30 dark:active:border-white/20 dark:hover:border-white/50 dark:active:text-white/50 shadow shadow-black/5 active:shadow-black/0 rounded-xl transition text-white">
-          <span>Buy Directly</span>
+          <span>
+            <Link to="https://stripe.com/en-in">
+           
+              Buy Directly
+               </Link>
+          </span>
           <span className="px-2.5 py-1 text-sm text-white dark:text-black bg-black dark:bg-white/90 rounded-full">$5.99</span>
         </button>
       </div>
