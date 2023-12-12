96b85b08380c15a4c6cc9a757fbdf29de78e5b9f
pavan <siriyalapavankumar@gmail.com>
2023-12-12 14:37:45 +0530

    customization page✅

--- src/components/Customize.jsx
+++ src/components/Customize.jsx
@@ -2,11 +2,11 @@ import TrailImage  from "../../images/Trail2.png"
 
 const Customize = () => {
   return (
-      <div className="flex flex-col items-center gap-8 mb-16">
+      <div className="flex flex-col items-center gap-8 mb-16 mt-20">
           <small className="inline-flex gap-2 font-semibold text-[#783AF5] dark:text-[#9C6AFF] uppercase tracking-wider">
               Customization
           </small>
-          <h2 className="text-3xl md:text-5xl font-bold">
+          <h2 className="text-3xl md:text-5xl font-bold mt-10">
               Countless ways to customize.
           </h2>
           <p className="text-lg md:text-2xl max-w-4xl">
